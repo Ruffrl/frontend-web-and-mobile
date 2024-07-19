b186f6d807dfde07c217d08e0083513c9ad5053a
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-07-19 09:53:50 -0500

    [#1] Readd react native web project

---
+++ desktop-mobile-web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import './App.css';
+
+import * as React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello from react native web</Text>
+    </View>
+  );
+}
+
+export default App;
